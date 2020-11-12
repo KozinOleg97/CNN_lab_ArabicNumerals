42a1035ae1823d4fe0f2897b14249d21f846705d
DenisSokolov96 <dens111296@gmail.com>
2020-11-12 17:51:24 +0300

    определиться с тестами

--- Main.py
+++ Main.py
@@ -1,11 +1,10 @@
 import PySimpleGUI as sg
-import splitfolders
 import numpy as np
 
 from Running import *
 
-listTrain = []
-listTest = []
+listTrain = np.zeros((6600, 93, 13, 1))
+listTest = np.zeros((2200, 93, 13, 1))
 
 
 def readFile():
@@ -13,9 +12,9 @@ def readFile():
     testData = 'D:\PytCharmProjects\CNNArabicNumerals\Datas\Test_Arabic_Digit.txt'
     try:
         global listTrain
-        listTrain = read(trainData, 661)
+        listTrain = read(trainData, 6600)
         global listTest
-        listTest = read(testData, 221)
+        listTest = read(testData, 2200)
     except IOError:
         print("No file")
         return False
@@ -24,62 +23,58 @@ def readFile():
 
 
 def read(strDatas, zn):
-    list = []
+    list = np.zeros((zn, 93, 13, 1))
     with open(strDatas, ) as str:
-        count = 0
-        iClass = 0
+        # iClass = 0
         iBlocks = -1
-        list.append([])
+        iStr = 0
         for line in str:
             s = line.splitlines()
             if len(s[0]) > 15:
                 strData = s[0].split(" ")
                 arr = np.array(strData, dtype=np.float32)
-                list[iClass][iBlocks].append(arr)
-                #list[iBlocks].append(arr)
+                for i in range(0, 13):
+                    # list[iClass][iBlocks][iStr][i] = arr[i]
+                    list[iBlocks][iStr][i] = arr[i]
+                iStr += 1
             else:
-                # можно удалить
-                #iBlocks += 1
-                #if iBlocks < 6599:
-                #    list.append([])
-                count += 1
-                if count < zn:
-                    iBlocks += 1
-                    list[iClass].append([])
-            if count == zn:
-                count = 1
-                iClass += 1
-                iBlocks = 0
-                list.append([[]])
+                # if iBlocks < zn:
+                iBlocks += 1
+                iStr = 0
+                # else:
+                #     iClass += 1
+                #     iBlocks = 0
+                #     iStr = 0
     return list
 
 
 def main():
-    Running.train(listTrain, 1)
-    # num_epochs = 1
-    # layout = [
-    #     [sg.Button("Обучить"), sg.Button("Распознать")],
-    #     [sg.Text("Эпохи"), sg.Input(key='-INPUT-', default_text="1", size=(7, 1), justification='center')],
-    #     [sg.Output(size=(88, 20), key='out')]
-    # ]
-    # window = sg.Window('CNN', layout)
-    # while True:
-    #     event, values = window.read()
-    #     if event == "Обучить" or event == "Распознать":
-    #         try:
-    #             num_epochs = int(values['-INPUT-'])
-    #         except ValueError:
-    #             print("Ошибка значения, выполняется для:" + str(num_epochs))
-    #     if event == "Обучить":
-    #         try:
-    #             Running.train(listTrain, num_epochs)
-    #         except Exception as inst:
-    #             print(inst)
-    #     elif event == "Распознать":
-    #         Running.test(listTest, num_epochs)
-    #     if event in (sg.WIN_CLOSED, 'Quit'):
-    #         break
-    # window.close()
+    #Running.train(listTrain, listTest, 1)
+    #Running.test(listTest)
+    num_epochs = 1
+    layout = [
+        [sg.Button("Обучить"), sg.Button("Распознать")],
+        [sg.Text("Эпохи"), sg.Input(key='-INPUT-', default_text="1", size=(7, 1), justification='center')],
+        [sg.Output(size=(88, 20), key='out')]
+    ]
+    window = sg.Window('CNN', layout)
+    while True:
+        event, values = window.read()
+        if event == "Обучить" or event == "Распознать":
+            try:
+                num_epochs = int(values['-INPUT-'])
+            except ValueError:
+                print("Ошибка значения, выполняется для:" + str(num_epochs))
+        if event == "Обучить":
+            try:
+                Running.train(listTrain, listTest, num_epochs)
+            except Exception as inst:
+                print(inst)
+        elif event == "Распознать":
+            Running.test(listTest)
+        if event in (sg.WIN_CLOSED, 'Quit'):
+            break
+    window.close()
 
 
 if __name__ == '__main__':
--- Running.py
+++ Running.py
@@ -1,22 +1,24 @@
 import tensorflow as tf
-
-from CNN import *
 import numpy as np
-from tqdm import tqdm
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Conv2D, AveragePooling2D, Activation, Flatten, Dropout, Dense, MaxPooling2D
-from tensorflow_core.python.keras.optimizers import SGD
-from tensorflow.python.keras.utils.data_utils import Sequence
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 class Running:
     @classmethod
-    def train(cls, listTrain, num_epochs):
+    def train(cls, listTrain, listTest, num_epochs):
+        train_answers = np.zeros((6600, 10))
+        test_answers = np.zeros((2200, 10))
+        train_answers = cls.fill_answers(train_answers, 93, 13)
+        test_answers = cls.fill_answers(test_answers, 93, 13)
+
         print("Обучение...")
         model = Sequential()
         model.add(Conv2D(32, kernel_size=(3, 3),
                          activation='relu',
-                         input_shape=( 93, 13, 1)))
+                         input_shape=(93, 13, 1)))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
         model.add(Flatten())
@@ -25,14 +27,11 @@ class Running:
         model.add(Dense(10, activation='softmax'))
         model.summary()
 
-        model.compile(optimizer='adam',
-                       loss='binary_crossentropy',
-                       metrics=['accuracy'])
-        y = []
-        for i in range(0, 10):
-            value = tf.keras.utils.to_categorical(i, 10)
-            y.append(value)
-        model.fit(listTrain, y, epochs=num_epochs)
+        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+        model.fit(listTrain, train_answers, epochs=num_epochs, validation_data=(listTest, test_answers))
+
+        model.save('Model')
 
         # model = Sequential()
         # # первый скрытый слой
@@ -69,51 +68,20 @@ class Running:
         # Оценка модели
         # model.evaluate_generator(generator=listTrain, steps=epochs)
 
-        # m = 5000
-        # num_classes = 10
-        # lr = 0.01
-        # beta1 = 0.95
-        # beta2 = 0.99
-        # img_dim = 28
-        # img_depth = 1
-        # f = 5
-        # num_filt1 = 8
-        # num_filt2 = 8
-        # batch_size = 32
-        #
-        #
-        # f1, f2, w3, w4 = (num_filt1, img_depth, f, f), (num_filt2, num_filt1, f, f), (128, 800), (10, 128)
-        # f1 = CNN.initializeFilter(f1)
-        # f2 = CNN.initializeFilter(f2)
-        # w3 = CNN.initializeWeight(w3)
-        # w4 = CNN.initializeWeight(w4)
-        #
-        # b1 = np.zeros((f1.shape[0], 1))
-        # b2 = np.zeros((f2.shape[0], 1))
-        # b3 = np.zeros((w3.shape[0], 1))
-        # b4 = np.zeros((w4.shape[0], 1))
-        #
-        # params = [f1, f2, w3, w4, b1, b2, b3, b4]
-        #
-        # cost = []
-        #
-        # print("LR:" + str(lr) + ", Batch Size:" + str(batch_size))
-        # train_data = np.array([listTrain[0], listTrain[1], listTrain[2], listTrain[3], listTrain[4]])
-        #
-        #
-        # for epoch in range(num_epochs):
-        #     np.random.shuffle(listTrain)
-        #     batches = [train_data[k:k + batch_size] for k in range(0, train_data.shape[0], batch_size)]
-        #
-        #     t = tqdm(batches)
-        #     for x, batch in enumerate(t):
-        #         params, cost = CNN.adamGD(batch, num_classes, lr, img_dim, img_depth, beta1, beta2, params, cost)
-        #         t.set_description("Cost: %.2f" % (cost[-1]))
-
-        # return cost
+    @classmethod
+    def fill_answers(cls, answers_list, block_size, data_len):
+        for i in range(0, data_len):
+            value = int(i / block_size)
+            value = tf.keras.utils.to_categorical(value, 10)
+            answers_list[i] = value
+        return answers_list
 
     @classmethod
-    def test(cls, listTest, num_epochs):
+    def test(cls, listTest):
         print("Тест...")
-        # test_set.reset()
-        # pred = model.predict_generator(test_set, steps=num_epochs, verbose=1)
+        model = tf.keras.models.load_model('Model')
+        test_answers = np.zeros((2200, 10))
+        test_answers = cls.fill_answers(test_answers, 93, 13)
+        score = model.evaluate(listTest, test_answers, verbose=0)
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
